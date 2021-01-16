46aa54e82fa73f0872087b1f401f08e5e1651d46
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-16 15:38:01 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,50 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize
+    @name = name
+    self.save
+    
+  end  
 
   def self.all
     @@all
   end
 
   def save
-    self.class.all << self
+    @@all
   end
+  
+  def self.create
+     song = Song.new 
+    song.save 
+    song
+  end  
+  
+  def self.new_by_name
+  end
+  
+  def self.create_by_name
+  end
+  
+  def self.find_by_name
+  end
+  
+  def self.find_or_create_by_name
+  end
+  
+  def self.alphabetical
+  end
+  
+  def self.new_from_filename
+  end
+  
+  def self.create_from_filename
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end  
 
 end
